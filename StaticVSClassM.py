a58430c71ff1fcfa48377d3fd5d5d9612100bdaf
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-18 17:05:25 -0700

    Update StaticVSClassM.py

--- StaticVSClassM.py
+++ StaticVSClassM.py
@@ -1,1 +1,19 @@
 
+class MyClass:
+    class_variable = 10
+
+    @staticmethod
+    def static_method(x, y):
+        return x + y
+
+    @classmethod
+    def class_method(cls, x):
+        return cls.class_variable * x
+
+result1 = MyClass.static_method(2, 3)
+
+result2 = MyClass.class_method(2)
+
+print(result1, result2)
+
+
